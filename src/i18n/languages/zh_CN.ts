a2b3dca64d2d7d96c67b493c39bf7b7ad5f2d7b0
孔大夫 <25993391+kongdf@users.noreply.github.com>
2025-04-07 11:09:39 +0800

    Update zh_CN.ts

--- src/i18n/languages/zh_CN.ts
+++ src/i18n/languages/zh_CN.ts
@@ -6,7 +6,7 @@ export const zh_CN: Translation = {
   [Key.about]: '关于我',
   [Key.archive]: '归档',
   [Key.search]: '搜索',
-  [Key.telegram]: 'Telegram',
+  [Key.telegram]: '随笔',
   [Key.tags]: '标签',
   [Key.categories]: '分类',
   [Key.recentPosts]: '最新文章',
